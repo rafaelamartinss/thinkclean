6010aa05408e3867c0ff53b8423cc15ab1890090
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-03-20 14:24:10 -0300

    create and add welcome section component

---
+++ components/AppWelcomeSection.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="welcome-area" id="welcome">
+        <div class="header-text">
+            <div class="container">
+                <div class="row">
+                    <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
+                        <h1>We provide the best <strong>cleaning service</strong> to your house, apartment and more!</h1>
+                        <a href="#features" class="main-button-slider">Book Your Cleaning Service!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="col-12">
         <AppNavbar />
+        <AppWelcomeSection />
         <AppCard />
     </div>
 </template>
@@ -8,8 +9,9 @@
 <script>
 import AppCard from "../components/AppCard.vue";
 import AppNavbar from "../components/AppNavbar.vue";
+import AppWelcomeSection from "../components/AppWelcomeSection.vue";
 
 export default {
-    components: { AppCard, AppNavbar }
+    components: { AppCard, AppNavbar, AppWelcomeSection }
 }
 </script>
