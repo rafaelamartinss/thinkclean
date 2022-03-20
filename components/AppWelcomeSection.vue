3e4aa6f6b13c14023bbe78b37a79e916f9b25336
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-03-20 14:29:59 -0300

    add welcome card component

--- components/AppWelcomeSection.vue
+++ components/AppWelcomeSection.vue
@@ -1,14 +1,34 @@
 <template>
-    <div class="welcome-area" id="welcome">
-        <div class="header-text">
-            <div class="container">
-                <div class="row">
-                    <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
-                        <h1>We provide the best <strong>cleaning service</strong> to your house, apartment and more!</h1>
-                        <a href="#features" class="main-button-slider">Book Your Cleaning Service!</a>
+    <div>
+        <div class="welcome-area" id="welcome">
+            <div class="header-text">
+                <div class="container">
+                    <div class="row">
+                        <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
+                            <h1>We provide the best <strong>cleaning service</strong> to your house, apartment and more!</h1>
+                            <a href="#features" class="main-button-slider">Book Your Cleaning Service!</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <section class="section home-feature">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <WelcomeCard />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
+<script>
+import WelcomeCard from './WelcomeCard.vue'
+
+export default {
+    components: { WelcomeCard }
+}
+</script>
---
+++ components/WelcomeCard.vue
@@ -0,0 +1,11 @@
+<template>
+     <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
+        <div class="features-small-item">
+            <div class="icon">
+                <i><img src="images/bucket (3).png" alt=""></i>
+            </div>
+            <h5 class="features-title">Professional Organizing</h5>
+            <p>Customize anything in this template to fit your website needs</p>
+        </div>
+    </div>
+</template>
