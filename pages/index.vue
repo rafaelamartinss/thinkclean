32adc4bb51380c0e3114f1af11a58ed2f0903131
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-03-20 11:27:23 -0300

    create appcard component and add to index view

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,13 @@
 <template>
-<div>
-    <!-- ***** Preloader Start ***** -->
-    <div id="preloader">
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>  
-    <!-- ***** Preloader End ***** -->
-    
-    
-    <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="/" class="logo">
-                            <!-- <img src="images/logo.png" alt="Softy Pinko"/> -->
-                            ThinkClean
-                        </a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li><a href="#welcome" class="active">Home</a></li>
-                            <li><a href="#features">About Us</a></li>
-                            <li><a href="#work-process">Window Cleaning</a></li>
-                            <li><a href="#testimonials">Testimonials</a></li>
-                            <li><a href="#contact-us">Contact Us</a></li>
-                        </ul>
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- ***** Header Area End ***** -->
-
-  <Tutorial/>
-    <!-- ***** Footer Start ***** -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p class="copyright">Copyright &copy; 2021 ThinkClean Company - Design: TemplateMo</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <div class="col-12">
+        <AppCard />
     </div>
 </template>
+
+<script>
+import AppCard from "../components/AppCard.vue";
+
+export default {
+    components: { AppCard }
+}
+</script>
